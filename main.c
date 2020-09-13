0bd3ac3b09cd494dda38cc8f95cca0c05c5068b5
yahyakharrazi <kharrazi1031yahya@gmail.com>
2020-09-13 19:55:27 +0100

    last bout v3

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ struct Card{
 	int value;
 	char type;
 	char color;
-	char img;
-	char * label;
+	char img;// icon
+	char * label; // table of characters for display K - A - J - 8
 };
 
 typedef struct Element Element;
@@ -27,12 +27,13 @@ void color(int t, int f){
     SetConsoleTextAttribute(H, f * 16 + t);
 }
 
+//prototype of functions
 Element* newNode(Card );
 Card newCard(int , char );
 int isEmpty(Element* );
-void insert(Element**, Card);
-Card pop(Element**);
-int peek(Element*);
+void insert(Element**, Card);//push
+void pop(Element**);
+Card peek(Element*);
 void displayCard(Card);
 void display(Element**, char *);
 Element * generateDeck();
@@ -42,56 +43,62 @@ void createColumn(Element**,Element *);
 void moveCard(Element **, Element **);
 int moveCardRest(Element **, Element **);
 Element * getHead(Element **);
-int validZone1(char c1, char * col);
-int validZone2(char c1, char * col);
+int validZone1(char c1, char * col);//validate the choice of the column if exists in zone 1
+int validZone2(char c1, char * col);//
 int validZone3(char c1, char * col);
 
 int main(){
-	srand(time(0));
-	system("color a5");
-    color(15, 10);
+	srand(time(0));//initialize random numbers
+	system("color a5");// background color of the window
+    color(15, 10);// 15 : color of font  10: background of font
     
     //declaration
     Element *zone1[8]={NULL}, *zone2[4]={NULL}, *zone3[4]={NULL}, *deck = NULL;
-    int moves=0, i,c1Index,c2Index,c1zone,c2zone,flags;
-	char c1,c2,Columns[]= {'Q','W','E','R','T','Y','U','I'}, choices[]={'A','B','C','D','Z','L','F'};
+    int moves=0, i,flags;// moves performed
+	int c1Index,c2Index,c1zone,c2zone;// index of choice in the zone - c1zone is the zone concerned for the move
+	char c1,c2; // choice stored in char
+	char Columns[]= {'Q','W','E','R','T','Y','U','I'};// chars in zone1
+	char choices[]= {'A','B','C','D','Z','L','F'};// chars of other choices
 	
-	//
+	//creating deck of 52 shuffled cards 
 	deck = generateDeck();
+	//generate 8 columns 7 for the first 4 columns and 6 for the last 4 columns
 	createColumn(zone1,deck);
 
 	do{
-		color(15, 10);
-		displayHeader(zone2, zone3);
+		//code for display
+		color(15, 10);//maintaining color 
+		displayHeader(zone2, zone3);//displaying zone 2 and zone 3
     	color(15, 10);
-		display(zone1,Columns);
-						
+		display(zone1,Columns);//displaying zone 1
 		color(15, 10);
+		
+		//displaying possible choices
 		printf("\t Choices: Q-W-E-R-T-Y-U-I - A-B-C-D - Z(Zone3) - F(Fin) - L(New) \n\n");
-		flags=0;
 		
 		do{
+			flags=0;// flags to stop while loop when F character is chosed
 			c1zone=0;
 			color(15, 10);
 			printf("Choose ur card position : ");
-			c1 = getch();
+			c1 = getch();//get character from user
 			printf("%c",c1);
-			if(validZone1(c1,Columns)!=-1){
-				c1zone=1;
-				c1Index = validZone1(c1,Columns);
-				flags=1;
+			if(validZone1(c1,Columns)!=-1){//-1 means the character doesn't exist
+				c1zone=1;//determines the zone of character 
+				c1Index = validZone1(c1,Columns);//retrieve index of column in the zone
+				flags=1;//triggered when a valid 
 			}
 			else if(validZone2(c1,choices)!=-1 || validZone2(c1,choices)!=4){
-				c1Index = validZone2(c1,Columns);
-				if(c1Index<4)			
-					c1zone=2;
+			//if the character is in zone 2 and not in zone3 because we cant move from zone 3 (see game rules)
+				c1Index = validZone2(c1,Columns);//retrieve index of column in the zone
+				c1zone=2;
 				flags=1;
 			}
 			else{
 				printf("\nplease choose again\n");
 			}	
-		}while(flags==0);
-		if(c1=='F')
+		}while(flags==0);//loops until flags is 1 which means valid character
+		if(c1=='F')//end of game
 			break;
 		do{
 			c2zone=0;
@@ -119,29 +126,31 @@ int main(){
 		
 		if(c1zone==1 && c2zone==1){
 			moveCard(&zone1[c1Index],&zone1[c2Index]);
-			printf("card moved \n");														
+			moves++;
+			printf("card moved\n");												
 		}
-		else if(c1zone==1 && c2zone==2)
+		else if(c1zone==1 && c2zone==2){
 			moveCard(&zone1[c1Index],&zone2[c2Index]);
+			moves++;
+		}
 		else if(c1zone==1 && c2zone==3){
 			if(moveCardRest(&zone1[c1Index],zone3)==1)
-				moves++;
+				moves++;//incrementing in case the move was successful
 			else
-				printf("invalid move\n");
-						
+				printf("invalid move\n");		
 		}
 		else if(c1zone==2 && c2zone==1)
 			moveCard(&zone2[c1Index],&zone1[c2Index]);
 		else if(c1zone==2 && c2zone==2)
 			moveCard(&zone2[c1Index],&zone2[c2Index]);
 		else if(c1zone==2 && c2zone==3){
-			if(moveCardRest(&zone2[c1Index],zone3)==1)
-				moves++;
+			if(moveCardRest(&zone2[c1Index],zone3)==1)//
+				moves++;//
 			else
 				printf("invalid move\n");
 		}
 		
-		system("cls");
+		system("cls");//clear the text in window
 	}while(c1!='F');
 
 	printf("your moves : %d\n", moves);
@@ -151,33 +160,33 @@ int main(){
 }
 
 Element* newNode(Card data){
+	//memory allocation
 	Element* element = (Element*)malloc(sizeof(Element)); 
-    element->data.color = data.color; 
-    element->data.value = data.value; 
-    element->data.type = data.type; 
-    element->data.img = data.img; 
-    element->data.label = data.label; 
-    
+    //assinging card to stack
+	element->data = data;    
     element->next = NULL; 
     return element;
 };
 
 int isEmpty(Element* element){
-	return !element;	
+	return !element;//checking if empty
 }
 
 Card newCard(int value, char type){
 	Card c;
 	c.value = value;
 	c.type = type;
-	if(c.value<10)
-		c.label = c.value + '0';
+	if(c.value<10 && c.value!=1)
+		c.label = c.value + '0';//making integer readable as char
 	else{
+		//some unusual values in the games gets replaced by followed character
+		if(c.value==1) c.label="A";
 		if(c.value==10) c.label="10";
 		if(c.value==11) c.label="J";
 		if(c.value==12) c.label="Q";
 		if(c.value==13) c.label="K";
 	}
+	//checking the type of card to give the right color and shape
 	switch(type){
 		case 's': 
 			c.img = 5;
@@ -200,26 +209,26 @@ Card newCard(int value, char type){
 }
 
 Element *generateDeck(){
-	
-	Element * pile = NULL;
-	Card Pack[52]; 
-	int flags[52]={0};
+	//creation 52 cards
+	Element * deck = NULL;
+	Card Pack[52]; // container of 52 cards 
+	int flags[52]={0}; // checking if card in index is checked
     int j;
-    for(j=0; j<13; j++)	Pack[j] = newCard(j+1, 'h');
+    for(j=0; j<13; j++)	Pack[j] = newCard(j+1, 'h');//initialising 13 cards with same type
     for(j=0; j<13; j++) Pack[13+j] = newCard(j+1, 'c');
     for(j=0; j<13; j++) Pack[13*2+j] = newCard(j+1, 'd');
     for(j=0; j<13; j++) Pack[13*3+j] = newCard(j+1, 's');
 
-    int remaining = 52;
+	//generation of random 
+    int remaining = 52;//remaining cards
     int randNum,i,cpt=0;
     while(remaining!=0){
-    	randNum = rand()%remaining;
-//    	printf("%d\n", randNum);
+    	randNum = rand()%remaining;//generate random number
     	cpt=0;
     	for(i=0;i<52;i++){
     		if(flags[i]==0){
     			if(randNum == cpt){
-    				insert(&pile, Pack[i]);
+    				insert(&deck, Pack[i]);//insertion of card in the linked list
 					flags[i]=1;
     				remaining--;
 					break;    				
@@ -228,7 +237,7 @@ Element *generateDeck(){
 			}
 		}
 	}
-	return pile;
+	return deck;
 }
 
 void insert(Element** root, Card data) { 
@@ -272,38 +281,40 @@ void displayVert(Element **e){
 
 }
   
-Card pop(Element** root) { 
+void pop(Element** root) { 
     if (isEmpty(*root)){
-		Card c= {0};
-		return c;     	
+//		Card c= {0};
+//		return c;     	
+		return ;     	
 	} 
 
-        
     Element* temp = *root; 
     *root = (*root)->next; 
-    Card popped = temp->data; 
+//    Card popped = temp->data; 
     free(temp); 
   
-    return popped; 
+//    return popped; 
 } 
   
-int peek(Element* root) { 
-    if (isEmpty(root)) 
-        return INT_MIN; 
+Card peek(Element* root) { 
+    if (isEmpty(root)) {
+		Card c= {0};//returning card of value equals 0
+        return c;
+	}
     Card card = root->data;
-    
-    return card.value; 
+    return card;
 } 
 
 void displayCard(Card c){
+	//giving red color white background for red cards
 	if(c.color=='r')
 		color(12, 15);
 	else
-		color(0, 15);		
-	if(c.value<10)
-		printf("%c     %c\t", c.label, c.img);
+		color(0, 15);//assigning black color white background for black cards
+	if(c.value<10 && c.value!=1)
+		printf("%c     %c\t", c.label, c.img);//displaying numbers
 	else
-		printf("%s    %c\t", c.label, c.img);
+		printf("%s    %c\t", c.label, c.img);//displaying caracters
 }
 
 void displayHeader(Element ** z2, Element** z3){
@@ -338,12 +349,12 @@ void display(Element ** root, char * t){
 	int i;
 	Card c;
 	Element * e;
-	for(i=0;i<8;i++){
+	for(i=0;i<8;i++){//looping through the elements of zone 1
 		e = root[i];
 		printf("   %c  \t", t[i]);
 
-		if(isEmpty(e)){
-			printf("is empty!!!\n");
+		if(isEmpty(e)){//checking whether if null or not
+			printf("\n\n");
 			return;		
 		}
 		else{
@@ -355,10 +366,7 @@ void display(Element ** root, char * t){
 		}
 		color(15, 10);
 		printf("\n\n");
-
 	}
-	
-
 }
 
 Element * getHead(Element ** elmnt){
@@ -375,44 +383,50 @@ void createColumn(Element** c,Element * d){
 	for(i=0;i<4;i++){
 		//first 4 columns with 7 places
 		for(j=0;j<7;j++){
-			insert(&c[i], pop(&d));			
+			//check if peeked card is not null
+		//	if((peek(d)).value!=0){
+				insert(&c[i], peek(d));	
+				pop(&d);						
+		//	}
 		}
 	}
+	
 	for(i=4;i<8;i++){
 		//last 4 columns with 6 places
 		for(j=0;j<6;j++){
-			insert(&c[i], pop(&d));			
+			 //if((peek(d)).value!=0){
+				insert(&c[i], peek(d));
+				pop(&d);
+		//	}
 		}
 	}
 }
 
 void moveCard(Element ** s, Element ** d){
-	
-//	printf("ddssd\n");
-//	displayCard((*s)->data);
-	Card c = pop(s);	
+	Card c = peek(*s);
+	pop(s);	
 	printf("\n \n");
 	displayCard(c);
 	color(15, 10);
 	printf("\n\n");
 	insert(d,c);
 }
-
-int moveCardRest(Element ** s, Element ** d){
-//	printf("ddssd\n");
-//	displayCard((*s)->data);
-	Card c = pop(s);
+//&s s *s **s
+int moveCardRest(Element ** s, Element ** d){//moving card to zone 3 
+	Card c = peek(*s);//creation of a structure of type Card and assigning the head of the stack to it
+	pop(s);//remove card from zone
+	color(15, 10);
 	printf("\n \n");
-	displayCard(c);
+	//displayCard(c);
 	if(c.type=='h'){
 		if(d[0]!=NULL){
-			if(d[0]->data.value==c.value-1){
+			if(d[0]->data.value==c.value-1){// check if the moved card is following the head of zone 3
 				insert(&d[0],c);
 				return 1;
 			}
 				
 		}
-		else if(c.value==1){
+		else if(c.value==1){// check if the zone is empty
 			insert(&d[0],c);
 			return 1;		
 		}		
